ff073ceebe963cdc7d592a681a2eec8eca59e2e0
Rahul Ranjan <genirahul@gmail.com>
2015-09-04 20:02:03 +0530

    HttpUtil and UserUtil

---
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/util/HttpUtil.java
@@ -0,0 +1,250 @@
+package com.pratilipi.android.pratilipi_and.util;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by Rahul Ranjan on 9/3/2015.
+ */
+public class HttpUtil {
+
+    public static final String IS_SUCCESSFUL = "isSuccessful";
+    public static final String RESPONSE_STRING = "responseString";
+
+    private static int sResponseCode = -1;
+
+    private static final String LOG_TAG = HttpUtil.class.getSimpleName();
+
+    public static HashMap<String, String> makeGETRequest(String apiEndpoint, HashMap<String, String> requestParams){
+        HttpURLConnection connection = null;
+        BufferedReader bufferedReader = null;
+
+        boolean isSuccessful;
+
+        try {
+            Uri builtUri = Uri.parse(apiEndpoint).buildUpon()
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+
+            if (requestParams != null) {
+                for (Map.Entry<String, String> entry : requestParams.entrySet()) {
+                    connection.addRequestProperty(entry.getKey(),
+                            entry.getValue());
+                }
+            }
+
+
+            int status = connection.getResponseCode();
+            Log.e(LOG_TAG, "Response Code : " + status);
+            InputStream inputStream;
+            if(status >= 400){
+                Log.e(LOG_TAG, "Error Returned");
+                isSuccessful = false;
+                inputStream = connection.getErrorStream();
+            } else{
+                isSuccessful = true;
+                inputStream = connection.getInputStream();
+            }
+
+            StringBuffer buffer = new StringBuffer();
+            if( inputStream == null ){
+                return null;
+            }
+
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                buffer.append(line + "\n");
+            }
+
+            bufferedReader.close();
+
+            if( buffer.length() == 0 )
+                return null;
+
+            Log.w(LOG_TAG, "Api response : " + buffer.toString());
+            HashMap<String, String> returnMap = new HashMap<>(2);
+            returnMap.put(IS_SUCCESSFUL, String.valueOf(isSuccessful));
+            returnMap.put(RESPONSE_STRING, buffer.toString());
+            return returnMap;
+
+        } catch (IOException e){
+            Log.e(LOG_TAG, "Error ", e);
+        }
+        return null;
+    }
+
+    public static HashMap<String, String> makePOSTRequest(String apiEndpoint, HashMap<String, String> requestParams){
+        HttpURLConnection connection = null;
+        BufferedReader bufferedReader = null;
+
+        boolean isSuccessful;
+
+        try {
+
+            Uri builtUri = Uri.parse(apiEndpoint).buildUpon()
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+
+            HashMap<String, String> payloadMap = requestParams;
+            if (payloadMap != null) {
+                String payload = "";
+                for (Map.Entry<String, String> entry : payloadMap
+                        .entrySet()) {
+                    payload += entry.getKey() + "=" + entry.getValue()
+                            + "&";
+                }
+                DataOutputStream wr = new DataOutputStream(
+                        connection.getOutputStream());
+                wr.writeBytes(payload);
+                wr.flush();
+                wr.close();
+            }
+
+            int status = connection.getResponseCode();
+            InputStream inputStream;
+            if(status >= 400){
+                Log.e(LOG_TAG, "Error Returned");
+                isSuccessful = false;
+                inputStream = connection.getErrorStream();
+            } else{
+                isSuccessful = true;
+                inputStream = connection.getInputStream();
+            }
+
+            StringBuffer buffer = new StringBuffer();
+            if( inputStream == null ){
+                return null;
+            }
+
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                buffer.append(line + "\n");
+            }
+            bufferedReader.close();
+            Log.v(LOG_TAG, "Api response : " + buffer.toString());
+
+            if( buffer.length() == 0 )
+                return null;
+
+            HashMap<String, String> returnMap = new HashMap<>(2);
+            returnMap.put(IS_SUCCESSFUL, String.valueOf(isSuccessful));
+            returnMap.put(RESPONSE_STRING, buffer.toString());
+            return returnMap;
+
+        } catch (IOException e){
+            Log.e(LOG_TAG, "Error ", e);
+        }
+        return null;
+    }
+
+    public static HashMap<String, String> makePUTRequest(String apiEndpoint, HashMap<String, String> requestParams) throws JSONException{
+
+        HttpURLConnection connection = null;
+        BufferedReader bufferedReader = null;
+
+        boolean isSuccessful;
+
+        try {
+
+            Uri builtUri = Uri.parse(apiEndpoint).buildUpon()
+                    .build();
+
+//            JSONObject jsonObject = new JSONObject();
+//            for (Map.Entry<String, String> entry : requestParams.entrySet()) {
+//                jsonObject.put(entry.getKey(), entry.getValue());
+//            }
+
+            String rawFormat = "{";
+            for (Map.Entry<String, String> entry : requestParams.entrySet()) {
+                rawFormat += "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\", ";
+            }
+            rawFormat += "}";
+
+            Log.e(LOG_TAG, "Raw Format : " + rawFormat);
+            URL url = new URL(builtUri.toString());
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+
+            Random random = new Random();
+            OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+            osw.write(String.format(rawFormat, random.nextInt(30), random.nextInt(20)));
+            osw.flush();
+            osw.close();
+
+            connection.connect();
+
+            Log.e(LOG_TAG, "URL : " + url.toString());
+//            Log.e(LOG_TAG, "JSON Object : " + jsonObject.toString());
+            sResponseCode = connection.getResponseCode();
+            Log.e(LOG_TAG, "Response Code : " + sResponseCode);
+            InputStream inputStream;
+            if(sResponseCode >= 400){
+                Log.e(LOG_TAG, "Error Returned");
+                isSuccessful = false;
+                inputStream = connection.getErrorStream();
+            } else{
+                isSuccessful = true;
+                inputStream = connection.getInputStream();
+            }
+
+            StringBuffer buffer = new StringBuffer();
+            if( inputStream == null ){
+                return null;
+            }
+
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                buffer.append(line + "\n");
+            }
+            bufferedReader.close();
+            Log.e(LOG_TAG, "Api response : " + buffer.toString());
+
+            if( buffer.length() == 0 )
+                return null;
+
+            connection.disconnect();
+
+            HashMap<String, String> returnMap = new HashMap<>(2);
+            returnMap.put(IS_SUCCESSFUL, String.valueOf(isSuccessful));
+            returnMap.put(RESPONSE_STRING, buffer.toString());
+            return returnMap;
+
+        } catch ( IOException e){
+            Log.e(LOG_TAG, "Error ", e);
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/util/UserUtil.java
@@ -0,0 +1,247 @@
+package com.pratilipi.android.pratilipi_and.util;
+
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.pratilipi.android.pratilipi_and.GetCallback;
+import com.pratilipi.android.pratilipi_and.data.PratilipiContract;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+/**
+ * Created by Rahul Ranjan on 9/2/2015.
+ */
+public class UserUtil {
+
+    private static final String LOG_TAG = UserUtil.class.getSimpleName();
+
+    private final String LOGIN_ENDPOINT = "http://www.pratilipi.com/api.pratilipi/oauth";
+    private final String REGISTER_ENDPOINT = "http://mark-4p41.www.prod-pratilipi.appspot.com/api.pratilipi/register";
+    private final String USER_PROFILE_ENDPOINT = "http://www.pratilipi.com/api.pratilipi/userprofile";
+
+    private final String HTTP_POST = "POST";
+    private final String HTTP_PUT = "PUT";
+
+    private static final String USER_NAME = "userName";
+    private static final String ACCESS_TOKEN_EXPIRY = "expiry";
+    private static final String ACCESS_TOKEN = "accessToken";
+
+    private boolean mIsSuccessful;
+    private ProgressDialog mProgressDialog;
+
+
+    public UserUtil(Context context, String processMesssage){
+        mProgressDialog = new ProgressDialog(context);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setMessage(processMesssage);
+    }
+
+    public void userLogin( HashMap<String, String> requestParams, GetCallback callback){
+        mProgressDialog.show();
+        new UserLoginAsyncTask(callback).execute(requestParams);
+    }
+
+    public void userRegister( HashMap<String, String> requestParams, GetCallback callback){
+        mProgressDialog.show();
+        new UserRegisterAsyncTask(callback).execute(requestParams);
+    }
+
+    public void getUserDetails( String data, GetCallback callback){
+        mProgressDialog.show();
+        new GetUserProfileAsyncTask(callback).execute(data);
+    }
+
+    public static void updateUser(Context context, String email, JSONObject responseJson)
+            throws JSONException {
+        Log.e(LOG_TAG, "updateUser function called");
+
+        saveAccessToken(context, responseJson.getString(ACCESS_TOKEN), responseJson.getLong(ACCESS_TOKEN_EXPIRY));
+
+        ContentValues values = new ContentValues();
+        values.put(PratilipiContract.UserEntity.COLUMN_DISPLAY_NAME, responseJson.getString(USER_NAME));
+        values.put(PratilipiContract.UserEntity.COLUMN_IS_LOGGED_IN, 1);
+
+        String where = PratilipiContract.UserEntity.COLUMN_EMAIL + "=?";
+        String[] selectionArgs = {email};
+
+        int rowsUpdated = context.getContentResolver().update(PratilipiContract.UserEntity.CONTENT_URI, values, where, selectionArgs);
+
+        if( rowsUpdated == 0 ){
+            values.put(PratilipiContract.UserEntity.COLUMN_EMAIL, email);
+            insertUser(context, values);
+        }
+
+    }
+
+    public static void insertUser( Context context, ContentValues values ) {
+        context.getContentResolver().insert(PratilipiContract.UserEntity.CONTENT_URI, values);
+    }
+
+    public static boolean saveAccessToken(Context context, String accessToken, Long expiry){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(ACCESS_TOKEN, accessToken);
+        editor.putString(ACCESS_TOKEN_EXPIRY, expiry.toString());
+        return editor.commit();
+    }
+
+    public static String getAccessToken(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(ACCESS_TOKEN, null);
+    }
+
+    private String makeServerCall (String apiEndpoint, String method, String requestData){
+        HttpURLConnection connection = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            Uri builtUri = Uri.parse(apiEndpoint).buildUpon()
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(method);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+
+            writer.write(requestData);
+            writer.flush();
+
+            int status = connection.getResponseCode();
+            Log.e(LOG_TAG, "Response Code : " + status);
+            InputStream inputStream;
+            if(status >= 400){
+                Log.e(LOG_TAG, "Error Returned");
+                mIsSuccessful = false;
+                inputStream = connection.getErrorStream();
+            } else{
+                mIsSuccessful = true;
+                inputStream = connection.getInputStream();
+            }
+
+            StringBuffer buffer = new StringBuffer();
+            if( inputStream == null ){
+                return null;
+            }
+
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                buffer.append(line + "\n");
+            }
+
+            writer.close();
+            bufferedReader.close();
+
+            if( buffer.length() == 0 )
+                return null;
+
+            Log.w(LOG_TAG, "Api response : " + buffer.toString());
+            return buffer.toString();
+
+        } catch (IOException e){
+            Log.e(LOG_TAG, "Error ", e);
+        }
+        return null;
+    }
+
+    private class UserLoginAsyncTask extends AsyncTask<HashMap<String, String>, Void, String> {
+
+        private GetCallback callback;
+
+        public UserLoginAsyncTask( GetCallback callback){
+            this.callback = callback;
+        }
+
+        @Override
+        protected String doInBackground(HashMap<String, String>... params) {
+            HashMap<String, String> responseMap = HttpUtil.makePOSTRequest(LOGIN_ENDPOINT, params[0]);
+            mIsSuccessful = Boolean.parseBoolean(responseMap.get(HttpUtil.IS_SUCCESSFUL));
+            return responseMap.get(HttpUtil.RESPONSE_STRING);
+        }
+
+        @Override
+        protected void onPostExecute(String responseString) {
+            mProgressDialog.hide();
+            mProgressDialog.dismiss();
+            callback.done(mIsSuccessful, responseString);
+            super.onPostExecute(responseString);
+        }
+    }
+
+    private class UserRegisterAsyncTask extends AsyncTask<HashMap<String, String>, Void, String> {
+
+        private GetCallback callback;
+
+        public UserRegisterAsyncTask( GetCallback callback){
+            this.callback = callback;
+        }
+
+        @Override
+        protected String doInBackground(HashMap<String, String>... params){
+            try {
+                HashMap<String, String> responseMap = HttpUtil.makePUTRequest(REGISTER_ENDPOINT, params[0]);
+                mIsSuccessful = Boolean.parseBoolean(responseMap.get(HttpUtil.IS_SUCCESSFUL));
+                return responseMap.get(HttpUtil.RESPONSE_STRING);
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String responseString) {
+            mProgressDialog.hide();
+            mProgressDialog.dismiss();
+            callback.done(mIsSuccessful, responseString);
+            super.onPostExecute(responseString);
+        }
+    }
+
+    private class GetUserProfileAsyncTask extends AsyncTask<String, Void, String> {
+
+        private GetCallback callback;
+
+        public GetUserProfileAsyncTask( GetCallback callback){
+            this.callback = callback;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String accessToken = params[0];
+            HashMap<String, String> requestParams = new HashMap<>(1);
+            requestParams.put(ACCESS_TOKEN, accessToken);
+            HashMap<String, String> responseMap = HttpUtil.makeGETRequest(USER_PROFILE_ENDPOINT, requestParams);
+            mIsSuccessful = Boolean.getBoolean(responseMap.get(HttpUtil.IS_SUCCESSFUL));
+            return responseMap.get(HttpUtil.RESPONSE_STRING);
+        }
+
+        @Override
+        protected void onPostExecute(String responseString) {
+            mProgressDialog.hide();
+            mProgressDialog.dismiss();
+            callback.done(mIsSuccessful, responseString);
+            super.onPostExecute(responseString);
+        }
+
+    }
+}
