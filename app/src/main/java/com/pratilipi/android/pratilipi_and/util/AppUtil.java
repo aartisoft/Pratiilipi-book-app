41ca979cb1a0e9d1a7db78a263f1dd935afb6165
Rahul Ranjan <genirahul@gmail.com>
2015-09-21 16:07:54 +0530

    Refactoring

--- app/src/main/java/com/pratilipi/android/pratilipi_and/util/AppUtil.java
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/util/AppUtil.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.text.format.Time;
-import android.util.Log;
 
 import com.pratilipi.android.pratilipi_and.LanguageSelectionActivity;
 
@@ -19,17 +18,20 @@ public class AppUtil {
     private static final long HINDI_LANGUAGE_ID = 5130467284090880L;
     private static final long TAMIL_LANGUAGE_ID = 6319546696728576L;
 
+    public static final String GUJARATI_LOCALE = "gu";
+    public static final String HINDI_LOCALE = "hi";
+    public static final String TAMIL_LOCALE = "ta";
+
 
     public static Long getPreferredLanguage(Context context){
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         String preferedLanguage =  prefs.getString(LanguageSelectionActivity.SELECTED_LANGUAGE,
                 LanguageSelectionActivity.SELECTED_LANGUAGE_GUJARATI);
-        Log.e(LOG_TAG, "Prefered Language : " + preferedLanguage);
-        if( preferedLanguage.equals("gu"))
+        if( preferedLanguage.equals(GUJARATI_LOCALE))
             return GUJARATI_LANGUAGE_ID;
-        else if ( preferedLanguage.equals("hi") )
+        else if ( preferedLanguage.equals(HINDI_LOCALE) )
             return HINDI_LANGUAGE_ID;
-        else if( preferedLanguage.equals("ta"))
+        else if( preferedLanguage.equals(TAMIL_LOCALE))
             return TAMIL_LANGUAGE_ID;
         else
             return 0L;
