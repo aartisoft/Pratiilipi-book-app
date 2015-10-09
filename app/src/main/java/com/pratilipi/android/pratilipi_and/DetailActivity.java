c3fcc71c2b4a610df4ec9a441d44a24a500dff43
Rahul Ranjan <genirahul@gmail.com>
2015-10-09 10:12:46 +0530

    OnClick function for "add to shelf" button Download Content function

--- app/src/main/java/com/pratilipi/android/pratilipi_and/DetailActivity.java
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/DetailActivity.java
@@ -2,18 +2,34 @@ package com.pratilipi.android.pratilipi_and;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
+import com.pratilipi.android.pratilipi_and.data.PratilipiContract;
 import com.pratilipi.android.pratilipi_and.datafiles.Pratilipi;
+import com.pratilipi.android.pratilipi_and.datafiles.User;
+import com.pratilipi.android.pratilipi_and.service.DownloadService;
+import com.pratilipi.android.pratilipi_and.util.PratilipiUtil;
+import com.pratilipi.android.pratilipi_and.util.ShelfUtil;
+import com.pratilipi.android.pratilipi_and.util.UserUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -21,14 +37,22 @@ public class DetailActivity extends AppCompatActivity {
     public static final String PRATILIPI = "pratilipi";
     public static final String PARENT_ACTIVITY_CLASS_NAME = "parentActivityClassName";
 
+    private Context mContext;
     private Pratilipi mPratilipi;
     private String mPratilipiId;
     private String mParentActivityClassName;
+    private JSONArray mIndexJsonArray;
+    private int mChapterCount;
+    private int mChapterNumber;
+    private int mPageCount;
+    private int mPageNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mContext = this;
 
         getSupportActionBar().setElevation(1f);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -69,6 +93,36 @@ public class DetailActivity extends AppCompatActivity {
             summaryTextView.setTypeface(typeFace);
             summaryTextView.setText(Html.fromHtml(mPratilipi.getSummary()));
         }
+
+        Button readButton = (Button) findViewById(R.id.detail_read_button);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "ReadButton Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button addToShelfButton = (Button) findViewById(R.id.detail_add_to_shelf_button);
+        addToShelfButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShelfUtil.addPratilipiToShelf(mContext, mPratilipi, new GetCallback() {
+                    @Override
+                    public void done(boolean isSuccessful, String data) {
+                        Log.e(LOG_TAG, "Add To Shelf Callback");
+                        Log.e(LOG_TAG, "Response Data : " + data);
+                        if(isSuccessful){
+                            User user = UserUtil.getLoggedInUser(mContext);
+                            UserUtil.incrementContentInShelfCount(mContext, user.getEmail());
+                            if(mPratilipi.getDownloadStatus() != 1){
+                                downloadContent(mPratilipiId);
+                            }
+                        }
+                        Toast.makeText(getBaseContext(), "Is Add To shelf Successful : " + isSuccessful, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 
     @Nullable
@@ -91,5 +145,114 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         return parentIntent;
+    }
+
+    private boolean downloadContent(String pratilipiId){
+        mPratilipiId = pratilipiId;
+        Uri uri = PratilipiContract.PratilipiEntity.getPratilipiByIdUri(pratilipiId);
+        Log.e(LOG_TAG, "Get Pratilipi By Id URI : " + uri.toString());
+        Cursor cursor = mContext.getContentResolver().query(uri, null, null, null, null);
+         if(cursor.moveToFirst()){
+            String contentType = cursor.getString(cursor.getColumnIndex(PratilipiContract.PratilipiEntity.COLUMN_CONTENT_TYPE));
+            if(contentType.equalsIgnoreCase(DownloadService.TEXT_CONTENT_TYPE)) {
+                String indexString = cursor.getString(cursor.getColumnIndex(PratilipiContract.PratilipiEntity.COLUMN_INDEX));
+                mPageCount = cursor.getInt(cursor.getColumnIndex(PratilipiContract.PratilipiEntity.COLUMN_PAGE_COUNT));
+                mPageNumber = 1;
+                Log.e(LOG_TAG, "Is index present : " + (indexString));
+                if (indexString == null || indexString.isEmpty()) {
+                    //WHEN INDEX IS NULL WHOLE CONTENT SHOULD BE UNDER CHAPTER 1
+                    mChapterCount = 1;
+                    mChapterNumber = 1;
+                    startDownloadService( DownloadService.TEXT_CONTENT_TYPE, pratilipiId, 0 );
+                } else{
+                    //WHEN INDEX IS NOT NULL
+                    try {
+                        mIndexJsonArray = new JSONArray(indexString);
+                        mChapterCount = mIndexJsonArray.length();
+                        mChapterNumber = 0;
+
+                        int chapterStartPageNo = mIndexJsonArray.getJSONObject(mChapterNumber).getInt("pageNo");
+                        Log.e(LOG_TAG, "Chapter Number / Page Number : " + (mChapterNumber + 1) + " / " + chapterStartPageNo);
+
+                        startDownloadService( DownloadService.TEXT_CONTENT_TYPE, pratilipiId, chapterStartPageNo );
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else{
+                //CONTENT_TYPE = IMAGE
+                mIndexJsonArray = null;
+                mChapterCount = 0;
+                startDownloadService( DownloadService.TEXT_CONTENT_TYPE, pratilipiId, 0 );
+            }
+
+        }
+
+        return false;
+    }
+
+    private class DownloadReceiver extends ResultReceiver {
+        public DownloadReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            super.onReceiveResult(resultCode, resultData);
+            if (resultCode == DownloadService.STATUS_CODE_SUCCESS) {
+                //Make request for next page
+                if( mPageCount > mPageNumber ){
+                    float completedPercentage = ((float)mPageNumber/(float)mPageCount) *100;
+                    Toast.makeText(mContext, completedPercentage + "% Completed", Toast.LENGTH_SHORT)
+                            .show();
+                    mPageNumber++;
+                    if(mIndexJsonArray == null){
+                        try{
+                            int chapterStartPageNo =  mIndexJsonArray.getJSONObject(mChapterNumber).getInt("pageNo");
+
+                            startDownloadService( DownloadService.TEXT_CONTENT_TYPE, mPratilipiId, chapterStartPageNo );
+
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    } else{
+                        startDownloadService( DownloadService.TEXT_CONTENT_TYPE, mPratilipiId, 0 );
+                    }
+
+                } else{
+                    //TODO : UPDATE PRATILIPI ENTITY is_downloaded = true
+                    PratilipiUtil.updatePratilipiDownloadStatus(
+                            mContext,
+                            mPratilipiId,
+                            PratilipiContract.PratilipiEntity.CONTENT_DOWNLOADED
+                    );
+                    Toast.makeText(mContext, "Download Completed", Toast.LENGTH_LONG)
+                            .show();
+                }
+            } else{
+                Toast.makeText(mContext, "Error Occured while downloading file.", Toast.LENGTH_LONG);
+            }
+        }
+    }
+
+    private void startDownloadService(String contentType,
+                                      String pratilipiId,
+                                      Integer chapterStartPageNumber){
+
+        //increment chapter number only for text content
+        if (contentType.equals(DownloadService.TEXT_CONTENT_TYPE)
+                && mPageNumber >= chapterStartPageNumber
+                && mChapterNumber < mChapterCount)
+            mChapterNumber++;
+
+        Log.e(LOG_TAG, "Page Number : " + mPageNumber);
+        Intent intent = new Intent(mContext, DownloadService.class);
+        intent.putExtra(DownloadService.INTENT_EXTRA_CONTENT_TYPE, contentType);
+        intent.putExtra(DownloadService.INTENT_EXTRA_CHAPTER_NUMBER, mChapterNumber);
+        intent.putExtra(DownloadService.INTENT_EXTRA_PRATILIPI_ID, pratilipiId);
+        intent.putExtra(DownloadService.INTENT_EXTRA_PAGE_NUMBER, mPageNumber);
+        intent.putExtra(DownloadService.INTENT_EXTRA_RECEIVER, new DownloadReceiver(new Handler()));
+        startService(intent);
     }
 }
