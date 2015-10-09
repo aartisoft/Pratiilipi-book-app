b216d4aa89505b3462f046b927591b88eceb5c26
Rahul Ranjan <genirahul@gmail.com>
2015-10-09 10:13:42 +0530

    Download service ContentUtil

---
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/service/DownloadService.java
@@ -0,0 +1,92 @@
+package com.pratilipi.android.pratilipi_and.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.ResultReceiver;
+import android.util.Log;
+
+import com.pratilipi.android.pratilipi_and.util.ContentUtil;
+import com.pratilipi.android.pratilipi_and.util.HttpUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+/**
+ * Created by Rahul Ranjan on 10/1/2015.
+ */
+public class DownloadService extends IntentService {
+
+    private static final String LOG_TAG = DownloadService.class.getSimpleName();
+    private static final String IMAGE_CONTENT_ENDPOINT = "http://www.pratilipi.com/api.pratilipi/pratilipi/content/image";
+    private static final String TEXT_CONTENT_ENDPOINT = "http://www.pratilipi.com/api.pratilipi/pratilipi/content";
+
+    private static final String PRATILIPI_ID = "pratilipiId";
+    private static final String PAGE_NUMBER = "pageNo";
+
+    public static final String INTENT_EXTRA_CONTENT_TYPE = "contentType";
+    public static final String INTENT_EXTRA_PAGE_NUMBER = "pageNumber";
+    public static final String INTENT_EXTRA_CHAPTER_NUMBER = "chapterNumber";
+    public static final String INTENT_EXTRA_PRATILIPI_ID = "pratilipiId";
+    public static final String INTENT_EXTRA_RECEIVER = "receiver";
+    public static final String TEXT_CONTENT_TYPE = "pratilipi";
+    public static final String IMAGE_COTENT_TYPE = "image";
+    public static final int STATUS_CODE_SUCCESS = 200;
+    public static final int STATUS_CODE_FAILURE = 500;
+
+    public DownloadService(){
+        super("DownloadService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String contentType = intent.getStringExtra(INTENT_EXTRA_CONTENT_TYPE);
+        String pratilipiId = intent.getStringExtra(INTENT_EXTRA_PRATILIPI_ID);
+        int pageNumber = intent.getIntExtra(INTENT_EXTRA_PAGE_NUMBER, 0);
+        int chapterNumber = intent.getIntExtra(INTENT_EXTRA_CHAPTER_NUMBER, 0);
+        ResultReceiver receiver = (ResultReceiver) intent.getParcelableExtra(INTENT_EXTRA_RECEIVER);
+
+//        Bundle bundle = intent.getExtras();
+//        for (String key : bundle.keySet()) {
+//            Object value = bundle.get(key);
+//            Log.e(LOG_TAG, String.format("%s %s (%s)", key,
+//                    value.toString(), value.getClass().getName()));
+//        }
+
+        String uriString;
+        if(contentType.equals(IMAGE_COTENT_TYPE))
+            uriString = IMAGE_CONTENT_ENDPOINT;
+        else
+            uriString = TEXT_CONTENT_ENDPOINT;
+
+        if(pageNumber == 0 ){
+            Log.e(LOG_TAG, "Page Number : " + pageNumber);
+            return;
+        }
+        HashMap<String, String> paramMap = new HashMap<>();
+        paramMap.put(PRATILIPI_ID, pratilipiId);
+        paramMap.put(PAGE_NUMBER, String.valueOf(pageNumber));
+
+        HashMap<String, String> responseMap = HttpUtil.makeGETRequest(this, uriString, paramMap);
+        if(Boolean.parseBoolean(responseMap.get(HttpUtil.IS_SUCCESSFUL))){
+            try {
+                JSONObject responseJson = new JSONObject(responseMap.get(HttpUtil.RESPONSE_STRING));
+                Boolean isSuccessfullyUpdateDB = ContentUtil.insert(this, responseJson, pratilipiId, String.valueOf(chapterNumber), String.valueOf(pageNumber));
+                if( isSuccessfullyUpdateDB )
+                    receiver.send(STATUS_CODE_SUCCESS, null);
+            } catch (JSONException e){
+                e.printStackTrace();
+                receiver.send(STATUS_CODE_FAILURE, null);
+            }
+        } else{
+            receiver.send(STATUS_CODE_FAILURE, null);
+        }
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/util/ContentUtil.java
@@ -0,0 +1,92 @@
+package com.pratilipi.android.pratilipi_and.util;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+
+import com.pratilipi.android.pratilipi_and.data.PratilipiContract;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Rahul Ranjan on 10/1/2015.
+ */
+public class ContentUtil {
+
+    private static final String TEXT_CONTENT = "pageContent";
+    private static final String IMAGE_CONTENT = "data";
+
+    public static boolean insert(Context context, JSONObject object, String pratilipiId, String chapterNumber, String pageNumber){
+
+        Uri returnUri = null;
+        //IF chapterNumber != null > Text content > Fetch and update DB
+        if (chapterNumber != null) {
+
+            String content = "";
+            try{
+                content = object.getString(TEXT_CONTENT);
+            }catch(JSONException e){
+                e.printStackTrace();
+            }
+
+            Uri uri = PratilipiContract.ContentEntity.getPratilipiContentByChapterUri(pratilipiId, chapterNumber);
+            Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+            if (cursor.moveToFirst()) {
+                //USED WHEN ONE CHAPTER IS CONTAINED IN MORE THAN 1 PAGE.
+                String existingContent = cursor.getString(cursor.getColumnIndex(PratilipiContract.ContentEntity.COLUMN_TEXT_CONTENT));
+                String updatedContent = existingContent + content.trim();
+                ContentValues values = new ContentValues();
+                values.put(PratilipiContract.ContentEntity.COLUMN_TEXT_CONTENT, updatedContent);
+                String selection = PratilipiContract.ContentEntity.COLUMN_PRATILIPI_ID + "=? AND "
+                                    + PratilipiContract.ContentEntity.COLUMN_CHAPTER_NUMBER + "=?";
+                String[] selectionArgs = new String[]{pratilipiId, chapterNumber};
+                int rowsUpdated = context.getContentResolver().update(PratilipiContract.ContentEntity.CONTENT_URI, values, selection, selectionArgs);
+                if(rowsUpdated > 0)
+                    return true;
+
+            } else {
+                //INSERT
+                ContentValues values = new ContentValues();
+                values.put(PratilipiContract.ContentEntity.COLUMN_PRATILIPI_ID, pratilipiId);
+                values.put(PratilipiContract.ContentEntity.COLUMN_CHAPTER_NUMBER, chapterNumber);
+                values.put(PratilipiContract.ContentEntity.COLUMN_TEXT_CONTENT, content.trim());
+                returnUri = context.getContentResolver().insert(PratilipiContract.ContentEntity.CONTENT_URI, values);
+                if(returnUri != null)
+                    return true;
+            }
+        } else if(pageNumber != null){
+
+            byte[] content = null;
+            try{
+                content = object.getString(IMAGE_CONTENT).toString().getBytes();
+            }catch(JSONException e){
+                e.printStackTrace();
+            }
+
+            Uri uri = PratilipiContract.ContentEntity.getPratilipiContentByPageNumberUri(pratilipiId, pageNumber);
+            Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+
+            ContentValues values = new ContentValues();
+            if(cursor.moveToFirst()) {
+                //TO PREVENT DUPLICATE STORAGE OF IMAGES
+                values.put(PratilipiContract.ContentEntity.COLUMN_IMAGE_CONTENT, content);
+                String selection = PratilipiContract.ContentEntity.COLUMN_PRATILIPI_ID + "=? AND "
+                                    + PratilipiContract.ContentEntity.COLUMN_PAGE_NUMBER + "=?";
+                String[] selectionArgs = new String[]{pratilipiId, pageNumber};
+                int rowsUpdated = context.getContentResolver().update(PratilipiContract.ContentEntity.CONTENT_URI, values, selection, selectionArgs);
+                if(rowsUpdated > 0)
+                    return true;
+            } else{
+                values.put(PratilipiContract.ContentEntity.COLUMN_PRATILIPI_ID, pratilipiId);
+                values.put(PratilipiContract.ContentEntity.COLUMN_PAGE_NUMBER, pageNumber);
+                values.put(PratilipiContract.ContentEntity.COLUMN_IMAGE_CONTENT, content);
+                returnUri = context.getContentResolver().insert(PratilipiContract.ContentEntity.CONTENT_URI, values);
+                if (returnUri != null)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
