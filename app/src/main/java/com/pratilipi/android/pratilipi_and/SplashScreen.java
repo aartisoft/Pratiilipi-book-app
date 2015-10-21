734cdc0f4c1bbe7f57ef963ef96d6bb2938cd8ad
Rahul Ranjan <genirahul@gmail.com>
2015-10-22 04:13:01 +0530

    Free memory when Activity is destroyed

--- app/src/main/java/com/pratilipi/android/pratilipi_and/SplashScreen.java
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/SplashScreen.java
@@ -3,6 +3,9 @@ package com.pratilipi.android.pratilipi_and;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SplashScreen extends Activity {
 
@@ -11,8 +14,32 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        Log.e("SplashScreen", "onCreate function");
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.e("SplashScreen", "onDestroy function");
+        unbindDrawables(findViewById(R.id.splashscreen_linear_layout));
+        System.gc();
+    }
+
+    private void unbindDrawables(View view) {
+        if (view.getBackground() != null) {
+            view.getBackground().setCallback(null);
+        }
+        Log.e("SplashScreen", "unbindDrawables function");
+
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                unbindDrawables(((ViewGroup) view).getChildAt(i));
+            }
+            ((ViewGroup) view).removeAllViews();
+        }
     }
 }
