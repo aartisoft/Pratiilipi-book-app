047441e9e3f7fa63e46511783b8df675b99149f2
Rahul Ranjan <genirahul@gmail.com>
2015-10-28 10:29:52 +0530

    Change menu language option removed String constants for Internet availability check

--- app/src/main/java/com/pratilipi/android/pratilipi_and/MainActivity.java
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/MainActivity.java
@@ -115,8 +115,6 @@ public class MainActivity extends AppCompatActivity{
             Log.e(LOG_TAG, "Change Content Language option is clicked");
             Intent intent = new Intent(this, LanguageSelectionActivity.class);
             startActivity(intent);
-        } else if(id == R.id.action_change_menu_language){
-            Log.e(LOG_TAG, "Change Menu Language option is clicked");
         }
 
         return super.onOptionsItemSelected(item);
