612fd7f50ab3befdd8bb0b49b9fe7c2997a02b45
Rahul Ranjan <genirahul@gmail.com>
2015-12-10 18:19:31 +0530

    Crashlytics integration and left part of fb login integration

--- app/src/main/java/com/pratilipi/android/pratilipi_and/MainActivity.java
+++ app/src/main/java/com/pratilipi/android/pratilipi_and/MainActivity.java
@@ -136,4 +136,9 @@ public class MainActivity extends AppCompatActivity{
 //            searchViewButton.clearFocus();
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
